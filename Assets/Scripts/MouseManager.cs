e7119b0d5c77063b08f68cacbf2346b6b551f91f
Taiga Kiyotaki <t2421gm@gmail.com>
2019-03-21 12:00:46 +0900

    add Character Move

---
+++ Assets/Scripts/MouseManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MouseManager : MonoBehaviour
+{
+    public EventVector3 OnClickEnvironment;
+    public LayerMask clickableLayer;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 50, clickableLayer.value))
+        {
+            if (hit.collider.gameObject.tag == "Doorway")
+            {
+
+            }
+            else
+            {
+
+            }
+
+            //left click
+            if (Input.GetMouseButtonDown(0))
+            {
+                OnClickEnvironment.Invoke(hit.point);
+            }
+        }
+
+    }
+}
+
+
+[System.Serializable]
+public class EventVector3 : UnityEvent<Vector3> { }
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    //private Animator anim;
+    private NavMeshAgent agent;
+    void Awake()
+    {
+        //anim = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
